1fc4f6fd82971569d144a7c67473d014861080d1
Izaan Kamal <izaankml@gmail.com>
2018-09-16 14:35:00 -0400

    Update mayubot.py

--- mayubot.py
+++ mayubot.py
@@ -3,23 +3,19 @@
 
 
 """Simple Bot to send timed Telegram messages.
-
 # This program is dedicated to the public domain under the CC0 license.
-
 This Bot uses the Updater class to handle the bot and the JobQueue to send
 timed messages.
-
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
-
 Usage:
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
 from telegram.ext import Updater, CommandHandler
-from datetime import time
+import datetime
 import logging
 
 # Enable logging
@@ -47,20 +43,26 @@ def set_weeb_message(bot, update, args, job_queue, chat_data):
 
     update.message.reply_text('Alright you stupid noob')
 
+def time(bot, update):
+    rn = datetime.datetime.now().time()
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id, text = str(rn))
+
+
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
-
 def main():
     """Run bot."""
-    updater = Updater("<TOKEN>")
+    updater = Updater("652731656:AAHu8RhL9yo1lVuUnXN6kL2vEqFEz-NW5M4")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
+    # on different commands - answer in
     #dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("time", time))
     dp.add_handler(CommandHandler("start", set_weeb_message,
                                   pass_args=True,
                                   pass_job_queue=True,
